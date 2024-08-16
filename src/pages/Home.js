ecc43128c9c42fa36dac612833a6da6636ab5f9f
Calvin <cv2k10@gmail.com>
2024-08-16 13:33:52 +0800

    add Routes, ProductContext, and UseContext products, filtered products on Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useContext, useState, useEffect} from 'react';
+import { ProductContext } from '../contexts/ProductContext';
 
 const Home = () => {
-  return <div>Homepage</div>;
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const products = useContext(ProductContext);
+  // console.log(products)
+
+  useEffect(() => {
+    setFilteredProducts(products.filter((product) => product.category === "men's clothing" || product.category === "women's clothing"))
+  }, [products])
+
+  useEffect(() => {
+    console.log(filteredProducts)
+  }, [filteredProducts])
+
+  return (
+  <div>
+      <section className="py-16">
+        <div className="container max-auto">
+          {filteredProducts.map((product, i) => (
+            <div key={i} className=''>{product.title}</div>
+          ))}
+        </div>
+    </section>
+  </div>);
 };
 
 export default Home;
